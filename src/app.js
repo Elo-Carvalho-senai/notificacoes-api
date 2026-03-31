26aaeae991b06e5433e6ae18c027fdec4fe14f0f
Raissa Fernandes <raissa.d.fernandes@aluno.senai.br>
2026-03-31 08:06:54 -0300

    atualizado

--- src/app.js
+++ src/app.js
@@ -2,29 +2,34 @@
 const express = require("express");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./swagger");
-const app = express();
 const cors = require("cors");
+
+// 👇 IMPORTA TODOS OS MIDDLEWARES PRIMEIRO
+const logger = require("./middlewares/logger");
 const responseTime = require("./middlewares/responseTime");
 const errorHandler = require("./middlewares/errorHandler");
-// ... todas as rotas acima ...
-// Middleware de rota não encontrada (sempre por último!)
 const notFound = require("./middlewares/notFound");
+
+const app = express();
+
 // Middlewares
 app.use(express.json());
 app.use(cors());
-app.use(logger);
+app.use(logger); // agora certo
 app.use(responseTime);
+
 // Documentação Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-const logger = require("./middlewares/logger");
-app.use(notFound);
+
 // Rotas
 const eventoRoutes = require("./routes/eventoRoutes");
 const participanteRoutes = require("./routes/participanteRoutes");
 const inscricaoRoutes = require("./routes/inscricaoRoutes");
+
 app.use("/eventos", eventoRoutes);
 app.use("/participantes", participanteRoutes);
 app.use("/inscricoes", inscricaoRoutes);
+
 // Rota raiz
 app.get("/", (req, res) => {
     res.json({
@@ -37,6 +42,9 @@ app.get("/", (req, res) => {
         },
     });
 });
-app.use(errorHandler); // Sempre por último!
+
+// 👇 SEMPRE POR ÚLTIMO
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
