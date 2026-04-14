9c6266950b82cdc14ed2324ba3bf30df2dc7f940
Elô Carvalho <maria.e.silva738@aluno.senai.br>
2026-04-14 09:06:48 -0300

    Aula 12 - banco de dados

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
 // src/server.js
-require("dotenv").config(); // Carrega as variáveis do .env
+require("dotenv").config();
 const app = require("./app");
+const sequelize = require("./config/database");
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-    console.log(`Ambiente: ${process.env.NODE_ENV || "development"}`);
-    console.log(`Documentação: http://localhost:${PORT}/api-docs`);
-});
+async function iniciar() {
+    try {
+        // Testar conexão com o banco
+        await sequelize.authenticate();
+        console.log("Conexão com MySQL estabelecida com sucesso!");
+        // Iniciar o servidor
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando em http://localhost:${PORT}`);
+            console.log(`Ambiente: ${process.env.NODE_ENV || "development"}`);
+            console.log(`Documentação: http://localhost:${PORT}/api-docs`);
+        });
+    } catch (erro) {
+        console.error("Erro ao conectar com o banco de dados:", erro.message);
+        process.exit(1);
+    }
+}
+iniciar();
