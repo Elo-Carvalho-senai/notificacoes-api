d718663d803e036b39ca0b78e603cfaea0845e88
Elô Carvalho <maria.e.silva738@aluno.senai.br>
2026-04-16 12:34:22 -0300

    Conectando com o banco

--- src/server.js
+++ src/server.js
@@ -1,18 +1,16 @@
+// src/server.js
 require("dotenv").config();
-
 const app = require("./app");
 const { sequelize } = require("./models");
 const PORT = process.env.PORT || 3000;
-
 async function iniciar() {
     try {
         await sequelize.authenticate();
         console.log("Conexão com MySQL estabelecida com sucesso!");
-        
         // Sincronizar Models com o banco (criar tabelas se não existirem)
         await sequelize.sync({ alter: true });
         console.log("Tabelas sincronizadas com o banco de dados.");
-        
+
         app.listen(PORT, () => {
             console.log(`Servidor rodando em http://localhost:${PORT}`);
             console.log(`Documentação: http://localhost:${PORT}/api-docs`);
