d8acb0cc5debdb5577f0c780f3202c123eb9f522
Raissa Fernandes <raissa.d.fernandes@aluno.senai.br>
2026-04-30 12:18:22 -0300

    atualiza evento

--- src/services/EventoService.js
+++ src/services/EventoService.js
@@ -1,63 +1,43 @@
-const EventoModel = require("../models/EventoModel");
+const { Evento } = require("../models");
 const { NotFoundError, ValidationError } = require("../errors/AppError");
-const {
-    isRequired,
-    isPositiveInteger,
-    minLength,
-    validar,
-} = require("../helpers/validators");
 
-function listarTodos() {
-    return EventoModel.listarTodos();
+async function listarTodos() {
+    const eventos = await Evento.findAll({
+        order: [["data", "ASC"]],
+    });
+    return eventos;
 }
 
-function buscarPorId(id) {
-    const evento = EventoModel.buscarPorId(id);
+async function buscarPorId(id) {
+    const evento = await Evento.findByPk(id);
+
     if (!evento) {
         throw new NotFoundError("Evento");
     }
+
     return evento;
 }
 
-function criar(dados) {
-    const { nome, descricao, data, local, capacidade } = dados;
-    // Validação
-    const erros = validar([
-        isRequired(nome, "Nome"),
-        isRequired(data, "Data"),
-        minLength(nome, 3, "Nome"),
-        isPositiveInteger(capacidade, "Capacidade"),
-    ]);
-    if (erros) {
-        throw new ValidationError(erros.join("; "));
+async function criar(dados) {
+    try {
+        const novoEvento = await Evento.create(dados);
+        return novoEvento;
+    } catch (erro) {
+        if (erro.name === "SequelizeValidationError") {
+            const mensagens = erro.errors.map(e => e.message).join("; ");
+            throw new ValidationError(mensagens);
+        }
+        throw erro;
     }
-    return EventoModel.criar({ nome, descricao, data, local, capacidade });
 }
 
-function atualizar(id, dados) {
-
-    const { nome, capacidade } = dados;
-    // Validações (campos opcionais no update)
-    const erros = validar([
-        minLength(nome, 3, "Nome"),
-        isPositiveInteger(capacidade, "Capacidade"),
-    ]);
-    if (erros) {
-        throw new ValidationError(erros.join("; "));
-    }
-    const eventoAtualizado = EventoModel.atualizar(id, dados);
-    if (!eventoAtualizado) {
-        throw new NotFoundError("Evento");
-    }
-    return eventoAtualizado;
+// Ainda não implementados (próxima aula)
+async function atualizar(id, dados) {
+    // TODO
 }
 
-function deletar(id) {
-    const deletado = EventoModel.deletar(id);
-    if (!deletado) {
-        throw new NotFoundError("Evento");
-    }
-    return true;
+async function deletar(id) {
+    // TODO
 }
 
 module.exports = {
